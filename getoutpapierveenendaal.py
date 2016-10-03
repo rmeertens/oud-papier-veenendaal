ef56d9014158f622815c5371cc94ea14934dc8d6
rmeertens <rolandmeertens@gmail.com>
2016-10-03 11:30:03 +0200

    site working

--- getoutpapierveenendaal.py
+++ getoutpapierveenendaal.py
@@ -33,13 +33,17 @@ print("\n\nEnd of list!!!\n\n")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim()
 for element in soup.findAll('table')[0].findAll('tr'):
+   desc = str(element.findAll("td")[0].findAll(text=True)[0]) 
  
+   desc+=" "+  str(element.findAll("td")[1].findAll(text=True)[0]) 
+
    location = str(element.findAll("td")[1].findAll(text=True)[0]) 
    location = geolocator.geocode(location + " veenendaal")
    if location != None:
     # print(location.address)
 #Flatiron Building, 175, 5th Avenue, Flatiron, New York, NYC, New York, ...
-     print((location.latitude, location.longitude))
+     #print((location.latitude, location.longitude))
+     print("{lat:"+str(location.latitude)+",lng:"+str(location.longitude)+",desc:'"+ desc+"'},")
 #(40.7410861, -73.9896297241625)
 
 
